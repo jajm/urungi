35a08ed4f70a76d0e10d510bfc5d947adf20f3cf
Julian Maurice <julian.maurice@biblibre.com>
2019-07-01 17:48:51 +0200

    Do not dropDatabase in test setup
    It's not necessary anymore

--- test/server/setup.js
+++ test/server/setup.js
@@ -1,18 +1,8 @@
-const mongoose = require('mongoose');
-const config = require('config');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
 module.exports = async () => {
-    mongoose.set('useNewUrlParser', true);
-    const connection = mongoose.createConnection(config.get('db'));
-    await connection.dropDatabase();
-
     // Make sure the mongod binary is downloaded before running tests
     const mongod = new MongoMemoryServer();
     await mongod.getConnectionString();
     await mongod.stop();
-
-    return new Promise(resolve => {
-        connection.close(resolve);
-    });
 };
